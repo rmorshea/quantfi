6adb53ecae7b7348581a61bd935b7dc6fee3518e
Ryan Morshead <ryan.morshead@gmail.com>
2014-09-05 15:38:39 -0700

    add brownian package

--- brownian/exp.py
+++ brownian/exp.py
@@ -1,69 +1,16 @@
-import random
-import numpy as np
+from numpy import arange
+from numpy import sqrt
+from numpy import exp
+from numpy import log
+from random import gauss
 
-def wiener(n, dt, t_init=0, w_init=0.0):
-    """Returns one realization of a Wiener process with n steps of length dt.
-    The time and Wiener series can be initialized using t_init and w_init respectively."""
-    n+=1
-    t_series = np.arange(t_init,n*dt,dt)
-    h = t_series[1]-t_series[0]
-    z = np.random.normal(0.0,1.0,n)
-    dw = np.sqrt(h)*z
-    dw[0] = w_init
-    w_series = dw.cumsum()
-    return t_series, w_series
-
-def get_dxs(x,lim=None):
-    '''returns a finite difference series based on the input data
-    
-    x = input data series
-    lim = returned differences are between +/-lim.'''
-    return [x[i+1]-x[i] for i in range(len(x)-1) if lim==None or abs(x[i+1]-x[i])<lim]
-
-def incr_res(T, W, c, mu=0, sigma=1):
-    '''Increase the resolution of a wiener series by a factor of c.
-
-    Returns a more reolved Wiener series and its associate time series
-    
-    T = the given Time series.
-    W = the associated Wiener series.
-    c = Scaling factor (integer greater than 1).
-    mu = Mean of W's underlying normal distribution.
-    sigma = Standard deviation of W's underlying normal distribution.
-    '''
-    dT = T[1]-T[0]
-    dt = float(T[1]-T[0])/c
-    t_series = []
-    w_series = []
-    for i in range(len(T)-1):
-        t = T[i]
-        w_t = W[i]
-        t_next = T[i+1]
-        w_next = W[i+1]
-        t_series.append(t)
-        w_series.append(w_t)
-        for j in range(c-1):
-            t+=dt
-            dW = (w_next-w_t)
-            sig = np.sqrt(t_next-t)*sigma
-            gaussian = np.exp(-dW**2/2/sig**2)/np.sqrt(2*np.pi)/sig
-            if gaussian<random.random()<(1-(t_next-t)/dT):
-                w_t+=np.abs(random.gauss(0,np.sqrt(dt)*sigma))*float(dW)/abs(dW)
-            else:
-                w_t+=random.gauss(0,np.sqrt(dt)*sigma)
-            t_series.append(t)
-            w_series.append(w_t)
-    t_series.append(T[-1])
-    w_series.append(W[-1])
-    return t_series,w_series
-
-def exp_estep(n, dt, a, b, m=1, mu=0, sigma=1, w_init=0, p_init=1, chain_length=0): 
+def exp_estep(n, dt, a, b, m=1, mu=0, sigma=1, w_init=0, p_init=1, chain_length=0):
     '''Moves a price series forward through n time steps using Euler's method.
     
     Details
     -------
     The Stochastic DE conatins a drift term and growth is reduced by noise.
-    Returns a price series along with its corisponding wiener and time series.
+    Returns a price series along with its corisponding Wiener and time series.
     
     Argumets
     --------
@@ -85,15 +32,15 @@ def exp_estep(n, dt, a, b, m=1, mu=0, sigma=1, w_init=0, p_init=1, chain_length=
     w_t = w_init
     p_t = p_init
     if chain_length>0:
-        t_series = np.arange(chain_length*dt,(n+chain_length-0.9)*dt,dt)
+        t_series = arange(chain_length*dt,(n+chain_length-0.9)*dt,dt)
         w_series = []
         p_series = []
     else:
-        t_series = np.arange(0,(n+0.1)*dt,dt)
+        t_series = arange(0,(n+0.1)*dt,dt)
         w_series = [w_init]
         p_series = [p_init]
     for i in range(n):
-        dw = random.gauss(mu,np.sqrt(dt)*sigma)
+        dw = gauss(mu,sqrt(dt)*sigma)
         # Incrament p(t) with dp as given by the DE.
         # Incrament w(t) with dw as given by the normal distribution
         p_t += (a*dt+b*dw)*p_t**(m)
@@ -103,13 +50,14 @@ def exp_estep(n, dt, a, b, m=1, mu=0, sigma=1, w_init=0, p_init=1, chain_length=
         p_series.append(p_t)
     return t_series,w_series,p_series
 
-def exp_astep(n, dt, a, b, m=1, mu=0, sigma=1, w_init=0, p_init=1, chain_length=0):
+def exp_astep(n, dt, a, b, mu=0, sigma=1, w_init=0, p_init=1, chain_length=0):
     '''Moves a price series forward through n time steps using an analytical method.
     
     Details
     -------
     The Stochastic DE conatins a drift term and growth is reduced by noise.
-    Returns a price series along with its corisponding wiener and time series.
+    Returns an exp type price series with its corisponding Wiener and time series.
+    Only analagous to exp_estep where m=1.
     
     Argumets
     ---------
@@ -117,7 +65,6 @@ def exp_astep(n, dt, a, b, m=1, mu=0, sigma=1, w_init=0, p_init=1, chain_length=
     dt = The size of the time steps
     a = Constant controling the significance of price drift
     b = Constant controling the significance of the noise
-    m = The default case refers to standard geometric brownian motion
     mu = Mean of the normal distribution
     sigma = Standard deviation of the normal distribution
     w_init = Initial value of the wiener series
@@ -131,34 +78,34 @@ def exp_astep(n, dt, a, b, m=1, mu=0, sigma=1, w_init=0, p_init=1, chain_length=
     w_t = w_init
     p_t = p_init
     if chain_length>0:
-        t_series = np.arange(chain_length*dt,(n+chain_length-0.9)*dt,dt)
+        t_series = arange(chain_length*dt,(n+chain_length-0.9)*dt,dt)
         w_series = []
         p_series = []
         trim = 0
     else:
-        t_series = np.arange(0,(n+0.1)*dt,dt)
+        t_series = arange(0,(n+0.1)*dt,dt)
         w_series = [float(w_init)]
         p_series = [float(p_init)]
         trim = 1
     for t in t_series[trim:]:
-        dw = random.gauss(mu,np.sqrt(dt)*sigma)
+        dw = gauss(mu,sqrt(dt)*sigma)
         w_t += dw
-        p_series.append(p_init*np.exp((a-0.5*b**2)*t + b*w_t))
+        p_series.append(p_init*exp((a-0.5*b**2)*t + b*w_t))
         w_series.append(w_t)
     return t_series,w_series,p_series
 
 def exp_etrace(series, x, y_init, dt, a, b, m=1, mu=0, sigma=1, chain=False):
-    """Returns a price/wiener series based on a retrace of a wiener/price series.
+    """Returns a price/wiener series by tracing over a wiener/price series.
     
     series = 'wiener'
     -----------------
-    Retraces the values of the wiener series to create a price series.
+    Traces a wiener series with Euler's method to make a price series.
     x = The retraced wiener series.
     y_init = Initial value of the price series.
     
     series = 'price':
     -----------------
-    Retraces the values of the price series to create a wiener series.
+    Traces an exp type price series with Euler's method to make a wiener series.
     x = The retraced price series.
     y_init = Initial value of the weiner series.
     
@@ -201,17 +148,17 @@ def exp_etrace(series, x, y_init, dt, a, b, m=1, mu=0, sigma=1, chain=False):
             w_series.append(w_t)
         return w_series
     
-def exp_atrace(series, x, dt, a, b, m=1, mu=0, sigma=1, p_init=1, chain=False):
+def exp_atrace(series, x, dt, a, b, mu=0, sigma=1, p_init=1, chain=False):
     """Returns a price/wiener series by tracing over a wiener/price series.
     
     series = 'wiener'
     -----------------
-    Retraces the values of the wiener series to create a price series.
+    Analyticaly traces a wiener series to make a price series.
     x = a wiener series.
     
     series = 'price':
     -----------------
-    Retraces the values of the price series to create a wiener series.
+    Analyticaly traces an exp type price series to make a wiener series.
     x = a price series.
     
     Parameters
@@ -219,7 +166,6 @@ def exp_atrace(series, x, dt, a, b, m=1, mu=0, sigma=1, p_init=1, chain=False):
     dt = The size of the time steps.
     a = Constant controling the significance of price drift.
     b = Constant controling the significance of the noise.
-    m = The default case refers to standard geometric brownian motion.
     mu = Mean of the normal distribution.
     sigma = Standard deviation of the normal distribution.
     p_init = Initial value of the price series.
@@ -239,7 +185,7 @@ def exp_atrace(series, x, dt, a, b, m=1, mu=0, sigma=1, p_init=1, chain=False):
         for t in [dt*j for j in range(i,len(w_series))]:
             alph = (a-0.5*b**2)*t
             beta = b*w_series[i]
-            p_series.append(p_init*np.exp(alph + beta))
+            p_series.append(p_init*exp(alph + beta))
             i+=1
         return p_series
     
@@ -253,6 +199,6 @@ def exp_atrace(series, x, dt, a, b, m=1, mu=0, sigma=1, p_init=1, chain=False):
             i = 0
         for t in [dt*j for j in range(i,len(p_series))]:
             alph = (a-0.5*b**2)*t
-            w_series.append((np.log(float(p_series[i])/p_init)-alph)/b)
+            w_series.append((log(float(p_series[i])/p_init)-alph)/b)
             i+=1
         return w_series
