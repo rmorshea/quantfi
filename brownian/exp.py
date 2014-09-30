d2edba93b21c2228d999b7e45ac1b0fcc9a4cdaf
Ryan Morshead <ryan.morshead@gmail.com>
2014-09-29 22:46:26 -0700

    methods for computing expected parameters based on data added

--- brownian/exp.py
+++ brownian/exp.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 from matplotlib.mlab import normpdf
 from IPython.html.widgets import interact
 from scipy.stats import ks_2samp
+from scipy.optimize import minimize
 
-def estep(n, dt, a, b, m=1, mu=0, sigma=1, w_init=0, p_init=1, chain_length=0):
+def estep(n, dt, mu, sigma, m=1, mnv=0, var=1, w_init=0, p_init=1, chain_length=0):
     '''Moves a price series forward through n time steps using Euler's method.
     
     Details
@@ -18,11 +19,11 @@ def estep(n, dt, a, b, m=1, mu=0, sigma=1, w_init=0, p_init=1, chain_length=0):
     --------
     n = The number of steps taken
     dt = The size of the time steps
-    a = Constant controling the significance of price drift
-    b = Constant controling the significance of the noise
+    mu = Constant controling the significance of price drift
+    sigma = Constant controling the significance of the noise
     m = The default case refers to standard geometric brownian motion
-    mu = Mean of the normal distribution
-    sigma = Standard deviation of the normal distribution
+    mnv = The mean value of the normal distribution
+    var = Standard deviation of the normal distribution
     w_init = Initial value of the wiener series
     p_init = Initial value of the price series. If chaining, give p_init of the
         previously existing series which the returned one will be linked to.
@@ -42,17 +43,17 @@ def estep(n, dt, a, b, m=1, mu=0, sigma=1, w_init=0, p_init=1, chain_length=0):
         w_series = [w_init]
         p_series = [p_init]
     for i in range(n):
-        dw = gauss(mu,np.sqrt(dt)*sigma)
+        dw = gauss(mnv,np.sqrt(dt)*var)
         # Incrament p(t) with dp as given by the DE.
         # Incrament w(t) with dw as given by the normal distribution
-        p_t += (a*dt+b*dw)*p_t**(m)
+        p_t += (mu*dt+sigma*dw)*p_t**(m)
         w_t += dw
         # Append new values to each series;
         w_series.append(w_t)
         p_series.append(p_t)
     return t_series,w_series,p_series
 
-def astep(n, dt, a, b, mu=0, sigma=1, w_init=0, p_init=1, chain_length=0):
+def astep(n, dt, mu, sigma, mnv=0, var=1, w_init=0, p_init=1, chain_length=0):
     '''Moves a price series forward through n time steps using an analytical method.
     
     Details
@@ -65,10 +66,10 @@ def astep(n, dt, a, b, mu=0, sigma=1, w_init=0, p_init=1, chain_length=0):
     ---------
     n = The number of steps taken
     dt = The size of the time steps
-    a = Constant controling the significance of price drift
-    b = Constant controling the significance of the noise
-    mu = Mean of the normal distribution
-    sigma = Standard deviation of the normal distribution
+    mu = Constant controling the significance of price drift
+    sigma = Constant controling the significance of the noise
+    mnv = The mean value of the normal distribution
+    var = Standard deviation of the normal distribution
     w_init = Initial value of the wiener series
     p_init = Initial value of the price series. If chaining, give p_init of the
         previously existing series which the returned one will be linked to.
@@ -90,24 +91,25 @@ def astep(n, dt, a, b, mu=0, sigma=1, w_init=0, p_init=1, chain_length=0):
         p_series = [float(p_init)]
         trim = 1
     for t in t_series[trim:]:
-        dw = gauss(mu,np.sqrt(dt)*sigma)
+        dw = gauss(mnv,np.sqrt(dt)*var)
         w_t += dw
-        p_series.append(p_init*np.exp((a-0.5*b**2)*t + b*w_t))
+        p_series.append(p_init*np.exp((mu-0.5*sigma**2)*t + sigma*w_t))
         w_series.append(w_t)
     return t_series,w_series,p_series
 
-def etrace(series, ts, x, a, b, y_init, m=1, mu=0, sigma=1, chain=False):
+def etrace(stype, ts, x, mu, sigma, y_init, m=1, mnv=0, vari=1, chain=False):
     """Returns a price/wiener series by tracing over a wiener/price series.
     
+    stype = The type of series provided for the argument x
     ts = The corrisponding time series to x where ts[0]=0
 
-    series = 'wiener'
+    stype = 'wiener'
     -----------------
     Traces a wiener series with Euler's method to make an exp type price series.
     x = The wiener series being retraced.
     y_init = Initial value of the price series.
     
-    series = 'price':
+    stype = 'price':
     -----------------
     Traces an exp type price series with Euler's method to make a wiener series.
     x = The price series being retraced.
@@ -115,17 +117,17 @@ def etrace(series, ts, x, a, b, y_init, m=1, mu=0, sigma=1, chain=False):
     
     Parameters
     ----------
-    a = Constant controling the significance of price drift
-    b = Constant controling the significance of the noise
+    mu = Constant controling the significance of price drift
+    sigma = Constant controling the significance of the noise
     m = The default case refers to standard geometric brownian motion
-    mu = Mean of the normal distribution
-    sigma = Standard deviation of the normal distribution
+    mnv = The mean value of the normal distribution
+    vari = Standard deviation of the normal distribution
     chain = Return the series excluding its initial value?
     
     (!) Note: it is suggested that all arguments be given as floats.
     """
     
-    if series == 'wiener':
+    if stype == 'wiener':
         p_t = y_init
         w_series = x
         if chain==True:
@@ -135,11 +137,11 @@ def etrace(series, ts, x, a, b, y_init, m=1, mu=0, sigma=1, chain=False):
         for i in range(1,len(w_series)):
             dt = ts[i]-ts[i-1]
             dw = w_series[i]-w_series[i-1]
-            p_t += (a*dt+b*dw)*p_t**(m)
+            p_t += (mu*dt+sigma*dw)*p_t**(m)
             p_series.append(p_t)
         return p_series
             
-    elif series == 'price':
+    elif stype == 'price':
         w_t = y_init
         p_series = x
         if chain==True:
@@ -149,39 +151,36 @@ def etrace(series, ts, x, a, b, y_init, m=1, mu=0, sigma=1, chain=False):
         for i in range(1,len(p_series)):
             dt = ts[i]-ts[i-1]
             dP = (p_series[i] - p_series[i-1])/(p_series[i-1]**(m))
-            w_t+=(dP-a*dt)/b
+            w_t+=(dP-mu*dt)/sigma
             w_series.append(w_t)
         return w_series
     
-def atrace(series, ts, x, a, b, mu=0, sigma=1, p_init=1, chain=False):
+def atrace(stype, ts, x, mu, sigma, p_init=1, chain=False):
     """Returns a price/wiener series by tracing over a wiener/price series.
     
+    stype = The type of series provided for the argument x
     ts = The corrisponding time series to x where ts[0]=0
 
-    series = 'wiener'
+    stype = 'wiener'
     -----------------
     Analyticaly traces a wiener series to make an exp type price series.
-    x = a wiener series.
+    x = A wiener series.
+    p_init = Initial value of the price series.
     
-    series = 'price':
+    stype = 'price':
     -----------------
     Analyticaly traces an exp type price series to make a wiener series.
-    x = a price series.
+    x = A price series.
     
     Parameters
     ----------
-    a = Constant controling the significance of price drift.
-    b = Constant controling the significance of the noise.
-    mu = Mean of the normal distribution.
-    sigma = Standard deviation of the normal distribution.
-    p_init = Initial value of the price series.
+    mu = Constant controling the significance of price drift.
+    sigma = Constant controling the significance of the noise.
     chain = Return the series excluding its initial value?
-    
-    (!) Note: it is suggested that all arguments be given as floats.
     """
     
     
-    if series == 'wiener':
+    if stype == 'wiener':
         p_series = []
         w_series = x
         if chain==True:
@@ -189,14 +188,15 @@ def atrace(series, ts, x, a, b, mu=0, sigma=1, p_init=1, chain=False):
         else:
             i = 0
         for t in ts:
-            alph = (a-0.5*b**2)*t
-            beta = b*w_series[i]
+            alph = (mu-0.5*sigma**2)*t
+            beta = sigma*w_series[i]
             p_series.append(p_init*np.exp(alph + beta))
             i+=1
         return p_series
     
     
-    if series == 'price':
+    if stype == 'price':
+        p_init = x[0]
         p_series = x
         w_series = []
         if chain==True:
@@ -204,12 +204,12 @@ def atrace(series, ts, x, a, b, mu=0, sigma=1, p_init=1, chain=False):
         else:
             i = 0
         for t in ts:
-            alph = (a-0.5*b**2)*t
-            w_series.append((np.log(float(p_series[i])/p_init)-alph)/b)
+            alph = (mu-0.5*sigma**2)*t
+            w_series.append((np.log(float(p_series[i])/p_init)-alph)/sigma)
             i+=1
         return w_series
 
-def ksplot(func,alims,blims,t_series,dat_series,bar_num,*func_auxargs):
+def ksplot(func,mu_lims,sig_lims,t_series,p_series,bar_num,*func_auxargs):
     """Compare traced data to a normal distribution with a KS test.
 
     Returns output of a KS test:
@@ -224,16 +224,16 @@ def ksplot(func,alims,blims,t_series,dat_series,bar_num,*func_auxargs):
     
     Arguments
     ---------
-    func = use etrace or atrace to act on dat_series and t_series
-    alims = slider limits for the paramter 'a' in func
-    blims = slider limits for the paramter 'b' in func
-    t_series = the time series corrisponding to dat_series
-    dat_series = the data set which will be traced
+    func = use etrace or atrace to act on p_series and t_series
+    mu_lims = slider limits for the paramter 'a' in func
+    sig_lims = slider limits for the paramter 'b' in func
+    t_series = the time series corrisponding to p_series
+    p_series = the data set which will be traced
     bar_num = number of bars in the histogramed plot
     func_auxargs = auxilary arguments for func
     """
     def makeplots(a,b):
-        dat = func('price',t_series,dat_series,a,b,*func_auxargs)
+        dat = func('price',t_series,p_series,a,b,*func_auxargs)
         fig, ax = plt.subplots(figsize=(10,2))
         ax.set_title('Data After Trace')
         ax.set_ylabel('wiener',fontsize=13)
@@ -244,11 +244,9 @@ def ksplot(func,alims,blims,t_series,dat_series,bar_num,*func_auxargs):
         dat_fds = np.array(get_fds(dat))
         dat_cnt,dat_mkr = np.histogram(dat_fds,bar_num)
         norm_hist = np.histogram(dat_fds,bar_num)[0]
-        norm_mkr = np.linspace(min(dat_mkr),max(dat_mkr),bar_num)
-        norm_cnt = normpdf(norm_mkr,0,np.std(dat_fds))
+        norm_cnt = normpdf(dat_mkr,0,np.std(dat_fds))
         norm_cnt = norm_cnt*max(dat_cnt)/max(norm_cnt)
-        norm_hist = np.histogram(norm_cnt,bar_num)[0]
-        ksstat,pval = ks_2samp(norm_hist, dat_cnt)
+        ksstat,pval = ks_2samp(norm_cnt, dat_cnt)
         print 'KS test using scaled fds of traced dat and a fit gaussian:'
         print 'Statistic value =',ksstat
         print 'Two sided p-value =',pval
@@ -260,9 +258,42 @@ def ksplot(func,alims,blims,t_series,dat_series,bar_num,*func_auxargs):
         
         wdth = float(max(dat_mkr)-min(dat_mkr))/bar_num
         plt.bar(dat_mkr[:-1],dat_cnt,label='traced fds (scaled by dt)',width = wdth,align='center')
-        plt.plot(norm_mkr,norm_cnt,label='fit gaussian',color='m')
+        plt.plot(dat_mkr,norm_cnt,label='fit gaussian',color='m')
         ax1.set_xlim(min(dat_mkr)-wdth,max(dat_mkr)+wdth)
         plt.legend(loc='best')
         plt.show()
 
-    interact(makeplots,a=alims,b=blims)
+    interact(makeplots,a=mu_lims,b=sig_lims)
+
+def ecomp_params(t_series,p_series):
+    '''Compute the expected parameters for the euler DE
+
+    Notes:
+    1) Calculations based on the assumption of small time steps
+    2) give time and data series as Pandas Series objects
+
+    t_series = the time series corrisponding to p_series
+    p_series = the data set which will be traced
+    '''
+    dt_inv = 1/np.mean((t_series-t_series.shift(1))[1:].values)
+    prev_vals = p_series.shift(1)
+    mu = dt_inv*np.mean(((p_series-prev_vals)/prev_vals)[1:])
+    sigma = dt_inv*np.var(((p_series-prev_vals)/prev_vals)[1:])
+    
+    return mu,sigma
+
+def acomp_params(t_series,p_series):
+    '''Compute the expected parameters of the analytical solution
+
+    Notes:
+    1) Calculations do not require small time steps
+    2) give time and data series as Pandas Series objects
+
+    t_series = the time series corrisponding to p_series
+    p_series = the data set which will be traced
+    '''
+    dt_inv = 1/np.mean((t_series-t_series.shift(1))[1:].values)
+    prev_vals = p_series.shift(1)
+    lnR = np.log(p_series/prev_vals)
+    sigma = dt_inv*np.var(lnR)
+    mu = dt_inv*np.mean(lnR)+sigma**2/2
